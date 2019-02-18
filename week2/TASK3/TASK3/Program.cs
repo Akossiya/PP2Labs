4e8f75a981bc367f5440e10e148cc3ed7204f28b
Aslan Kossiyabek <dj.mexico@mail.ru>
2019-02-18 23:07:05 +0600

    Added Task 3

---
+++ week2/TASK3/TASK3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TASK3
+{
+    class Program
+    {
+        static void PrintDir(DirectoryInfo aDir, int level)
+        {
+            foreach (FileInfo f in aDir.GetFiles())
+            {
+                Tabs(level);
+                Console.WriteLine(f.Name);  
+   
+            }
+            foreach (DirectoryInfo d in aDir.GetDirectories())
+            {
+                Tabs(level);
+                Console.WriteLine(d.Name);
+                PrintDir(d, level + 1);
+            }
+        }
+        public static void Tabs(int level)
+        {
+            for (int i = 0; i < level; i++)
+            {
+                Console.Write("    ");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // Создаем экземпляр dir для указанной папки
+            DirectoryInfo dir = new DirectoryInfo("/Users/User/Desktop/PP2Labs");
+
+            // Выводим содержимое папок с помощью рекурсивной функции
+            PrintDir(dir, 0);
+            
+            // Ждем нажатия любой клавиши, чтобы показать результат выполнения
+            Console.ReadKey();
+        }
+    }
+}
